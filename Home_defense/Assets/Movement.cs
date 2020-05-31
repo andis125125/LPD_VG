fd640d801f2f919e0b4e0604846f5ccb88be9800
andis125125 <andislazdins2@gmail.com>
2020-05-31 13:07:12 +0300

    Added player movement and house

---
+++ Home_defense/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 input = new Vector3(x, 0, z);
+        Vector3 localInput = transform.rotation * input;
+        Vector3 motion = localInput * speed * Time.deltaTime;
+
+        motion += Physics.gravity * Time.deltaTime;
+
+
+
+
+        characterController.Move(motion);
+    }
+}
---
+++ Home_defense/Assets/cameraMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivity = 100f;
+
+    [SerializeField]
+    Transform body;
+
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRot -= mY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+        body.Rotate(Vector3.up * mX);
+    }
+}
